bff6a562552a398e87a5076a82f8ff099aa337c2
yevheniikolisnyk <yevhenii.kolisnyk@gmail.com>
2022-09-26 09:20:43 +0200

    NSL-459

--- src/core/components/Table/TableCell.tsx
+++ src/core/components/Table/TableCell.tsx
@@ -268,26 +268,6 @@ const TableCellInternal = ({
             {(error || showExpandIcon) &&
             <div className={cellClasses.iconsTop}>
 
-                {selected && !disabled && showExpandIcon &&
-                <IconButton
-                    ref={iconRef}
-                    color={"inherit"}
-                    size={"small"}
-                    onClick={doOpenPopup}>
-                    <svg
-                        className={"MuiSvgIcon-root MuiSvgIcon-fontSizeSmall"}
-                        fill={"#666"}
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24">
-                        <path className="cls-2"
-                              d="M20,5a1,1,0,0,0-1-1L14,4h0a1,1,0,0,0,0,2h2.57L13.29,9.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L18,7.42V10a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1Z"/>
-                        <path className="cls-2"
-                              d="M10.71,13.29a1,1,0,0,0-1.42,0L6,16.57V14a1,1,0,0,0-1-1H5a1,1,0,0,0-1,1l0,5a1,1,0,0,0,1,1h5a1,1,0,0,0,0-2H7.42l3.29-3.29A1,1,0,0,0,10.71,13.29Z"/>
-                    </svg>
-                </IconButton>
-                }
-
                 {error && <Tooltip
                     classes={{
                         arrow: cellClasses.arrow,
